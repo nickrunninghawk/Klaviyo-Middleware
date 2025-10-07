8bec0c4e75552435008af5f8155d7564a413b920
Nick RunningHawk <nick.runninghawk@rustoleum.com>
2025-10-07 15:46:10 -0500

    Webhook verification

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // server.mjs (or package.json "type": "module")
 
 import express from "express";
+import { Webhook } from "standardwebhooks";
 
 const app = express();
+const CLIENT_SECRET = "4a4f15c9e88d77553dbaa18bf101cc9ba6f5fce8bbba9c2f86f6984fac0ae074";
+const webhook = new Webhook(Buffer.from(CLIENT_SECRET, "utf8").toString("base64"));
 
 // Simple routes
-app.get("/", (_, res) => res.send("Hello from Render 🚀"));
+app.get("/", express.raw({ type: "*/*" }), (_, res) => {
+
+    res.send("Hello from Render 🚀")
+});
 app.get("/health", (_, res) => res.send("ok"));
 
-app.post("/klaviyo", (req, res) => {
+app.post("/klaviyo", express.raw({ type: "*/*" }), (req, res) => {
     // Process the webhook payload here 
     console.log("Received Klaviyo webhook:", req.body);
     console.log("Headers:", req.headers);
+    const isVerified = webhook.verify(req.headers, req.body)
+    console.log("Webhook verified:", isVerified);
+
+
     res.status(200).send("Webhook received");
 });
 
